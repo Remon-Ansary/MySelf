9d0301778cda67c372851ecc0590805cdfec52f9
Remon Ansary <remonansary@gmail.com>
2021-01-26 16:11:10 +0600

    Arrow

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+
+
+ var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
